274e90005a5fa8c537eb803899ca7de01c315baf
kevmgb <kithinjimkevin@gmail.com>
2023-11-04 18:48:54 +0300

    style card components in home page

--- src/components/CardUpdated.js
+++ src/components/CardUpdated.js
@@ -45,21 +45,37 @@ import {
   MDBCardTitle,
   MDBCardText,
   MDBRow,
-  MDBCol
+  MDBCol,
+  MDBCardFooter,
+  MDBCardHeader,
+  MDBIcon
 } from 'mdb-react-ui-kit';
 import cardImage from '../images/house.jpg';
 import { Link } from 'react-router-dom';
 
 export default function CardUpdated({id, name, beds, baths}) {
   return (
-    <Link to={`/listing/detail/${id}`} >
+    <Link to={`/listing/detail/${id}`} style={{ color: 'inherit' }}>
       <MDBCol className='h-100'>
         <MDBCard className='h-100'>
             <MDBCardImage src={cardImage} alt='...' position='top'/>
+            <MDBCardHeader>{name}</MDBCardHeader>
             <MDBCardBody>
-            <MDBCardTitle>{name}</MDBCardTitle>
-            <MDBCardText>Placeholder</MDBCardText>
+              <MDBCardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </MDBCardText>
             </MDBCardBody>
+            <MDBCardFooter className='text-muted'>
+              <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+                <MDBCol className='col-md-8'>
+                  <small className='text-muted'>Posted 2 days ago</small>
+                </MDBCol>
+                <MDBCol className='col-md-2'>
+                  <MDBIcon fas icon="bed" /> {beds} 
+                </MDBCol>
+                <MDBCol className='col-md-2'>
+                  <MDBIcon fas icon="bath"/> {baths}
+                </MDBCol>
+              </MDBRow>   
+            </MDBCardFooter>
         </MDBCard>
       </MDBCol>
     </Link>
