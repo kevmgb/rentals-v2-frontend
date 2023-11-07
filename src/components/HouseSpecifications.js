fd7fd61b3ffa6f187d2d3aede283e465a622f3aa
kevmgb <kithinjimkevin@gmail.com>
2023-11-07 16:33:33 +0300

    add listing contact in listing page

--- src/components/HouseSpecifications.js
+++ src/components/HouseSpecifications.js
@@ -11,8 +11,8 @@ export default function HouseSpecifications({beds, bathrooms}) {
     <MDBCard>
       <MDBCardHeader>Specifications</MDBCardHeader>
       <MDBListGroup flush>
-        <MDBListGroupItem>Bedrooms    {beds}</MDBListGroupItem>
-        <MDBListGroupItem>Bathrooms   {bathrooms}</MDBListGroupItem>
+        <MDBListGroupItem>Bedrooms:    {beds}</MDBListGroupItem>
+        <MDBListGroupItem>Bathrooms:   {bathrooms}</MDBListGroupItem>
       </MDBListGroup>
     </MDBCard>
   );
