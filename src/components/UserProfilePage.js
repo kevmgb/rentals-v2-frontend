b6c45321b6359000d328755542727f238e5adb4a
kevmgb <kithinjimkevin@gmail.com>
2023-11-10 10:06:09 +0300

    implement view user profile and update user profile

--- src/components/UserProfilePage.js
+++ src/components/UserProfilePage.js
@@ -1,17 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  MDBContainer,
+  MDBBtn,
+  MDBInput,
+}
+from 'mdb-react-ui-kit';
+import axios from 'axios';
 
 function UserProfilePage() {
-  return (
-    <div>
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
 
-      <h2>User Profile Page</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at purus a ex ullamcorper semper.
-        Nulla facilisi. Maecenas eget viverra mi. Donec euismod dui at efficitur lacinia. Curabitur consequat
-        urna id enim dapibus auctor. Vestibulum et lacus ut sem interdum ultrices. Aenean posuere vehicula
-        ligula, non accumsan ex ultricies a. Quisque nec elit eget purus venenatis malesuada.
-      </p>
-    </div>
+  const handleProfileUpdateSubmit = (e) => {
+    
+    e.preventDefault();
+    
+    const token = localStorage.getItem('token');
+    axios.post("http://localhost:8080/api/v1/user/update", {
+      name: name,
+      email: email,
+    }, {
+        headers: {
+          "Content-Type": "application/json",
+          'Access-Control-Allow-Origin': '*',
+          'Authorization': `Bearer ${token}`
+        }
+      })
+    .then(response => {
+      // Handle successful response from the backend
+      console.log('User profile updated successfully', response.data);
+    })
+    .catch(error => {
+      // Handle error response from the backend
+      console.error('Error:', error);
+      alert("An erorr ocurred");
+    });
+  }
+
+  const loadUserProfile = () => {
+    axios.get("http://localhost:8080/api/v1/user")
+      .then(response => {
+        const { nameRes, emailRes } = response.data;
+        setName(nameRes);
+        setEmail(emailRes);
+      })
+      .catch(error => console.error('Error:', error));
+  };
+
+  useEffect(() => {
+    loadUserProfile();
+  }, []);
+
+  return (
+    <>
+      <div className='container-full' style={{ marginBottom: '40px' }}>
+        <div className="text-center mb-3 mt-4">
+          <h5>MY PROFILE:</h5>
+        </div>
+        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+            <div className="text-center mb-3">
+              <p>Manage your profile:</p>
+            </div>
+  
+            <MDBInput wrapperClass='mb-4' label='Name' id='typeText' type='text' value={name} onChange={handleNameChange}/>
+            <MDBInput wrapperClass='mb-4' label='Email' id='typeText' type='text' value={email} onChange={handleEmailChange}/>
+            
+            <form onSubmit={handleProfileUpdateSubmit}>
+              <MDBBtn className="mb-4 w-100">Update Profile</MDBBtn>
+            </form>
+            
+      </MDBContainer>
+      </div>
+    </>
+
   );
 }
 
